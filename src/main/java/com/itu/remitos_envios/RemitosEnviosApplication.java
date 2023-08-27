daf476df84b5fd0e3a61c9d6514a7882b485a406
Rodrigo I <itursarry@gmail.com>
2023-08-27 20:01:41 -0300

    Init repo

---
+++ src/main/java/com/itu/remitos_envios/RemitosEnviosApplication.java
@@ -0,0 +1,13 @@
+package com.itu.remitos_envios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemitosEnviosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemitosEnviosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itu/remitos_envios/RemitosEnviosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itu.remitos_envios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemitosEnviosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
